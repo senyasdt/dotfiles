bef5adf539a3a96abfb2d131e4c22cdce36f862b
senyasdt <senyasdt4745@gmail.com>
2026-04-10 14:20:34 +0300

    Cleanup

--- run_once_05_install_nix.sh
+++ run_once_05_install_nix.sh
@@ -1,5 +1,16 @@
-{{ if eq .chezmoi.os "linux-ubuntu" -}}
-#!/bin/bash
-if command -v apt >/dev/null 2>&1; then
-  sudo apt update && sudo apt upgrade -y
+#!/usr/bin/env bash
+set -e
+
+echo "==> Installing Nix"
+
+if ! command -v nix &>/dev/null; then
+  sh <(curl -L https://nixos.org/nix/install) --daemon
 fi
+
+# загрузка nix
+. /etc/profile.d/nix.sh || true
+
+echo "==> Enabling flakes"
+
+sudo mkdir -p /etc/nix
+echo "experimental-features = nix-command flakes" | sudo tee /etc/nix/nix.conf
