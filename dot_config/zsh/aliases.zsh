c8b2cd5c78274190e58633be6ac97bdbc799a357
senyasdt <senyasdt4745@gmail.com>
2026-04-10 11:52:01 +0300

    Add navi

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
 alias cat='bat -pp'
+alias ch=chezmoi
 
 if command -v eza >/dev/null; then
   alias ls="eza --long --all --group --icons=auto"
--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -3,3 +3,4 @@ export VISUAL="nvim"
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
 
 LS_COLORS="di=1;32:ln=36:ex=1;31:fi=0"
+# export NAVI_PATH=$HOME/.config/navi/cheats
