2bd9c4318e760040612dd6847c205142e478502a
senyasdt <senyasdt4745@gmail.com>
2026-04-11 22:40:42 +0300

    Fix zsh

--- run_once_20_install_ohmyzsh.sh
+++ run_once_20_install_ohmyzsh.sh
@@ -3,10 +3,9 @@ set -e
 
 echo "==> Installing oh-my-zsh"
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  nix profile install nixpkgs#git
-  git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-fi
+# if [ ! -d "$HOME/.oh-my-zsh" ]; then
+#   git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+# fi
 
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.config/zsh}"
 
@@ -18,3 +17,5 @@ if [ ! -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
     "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" \
     "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 fi
+
+chsh -s $(which zsh)
