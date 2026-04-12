7d4d4559e12e6816ae0f98fe184c0baeee30ea3b
senyasdt <senyasdt4745@gmail.com>
2026-04-13 02:59:03 +0300

    Fix zsh

--- dot_config/zsh/completion.zsh
+++ dot_config/zsh/completion.zsh
@@ -1,24 +1,10 @@
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format '[%d]'
 
-zstyle ':completion:*' completer _complete _complete:all
+zstyle ':completion:*:*:-command-:*:*' group-order alias builtins functions commands
+zstyle ':completion:*:complete:git-rebase:*' group-order 'commits'
 zstyle ':completion:*:*:git-checkout:*' group-order main-branches upstream-branches tags
 
-# zstyle ':completion:*:*:-command-:*' tag-order \
-#     'builtin-commands' \
-#     'functions' \
-#     'aliases' \
-#     'commands' \
-#     '*'
-#
-zstyle ':completion:*:*:-command-:*' group-order \
-    'builtin command' \
-    'shell function' \
-    alias \
-    'external command'
-
-zstyle ':completion:*:*:-command-:*:commands' ignored-patterns \
-    '*.dll' '*.DLL' '*.nls' '*.NLS'
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
 # set descriptions format to enable group support
@@ -27,7 +13,7 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 zstyle ':completion:*' menu no
 # preview directory's content with eza when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview '(( $+commands[eza] )) && eza -1 --color=always -- "$realpath" || ls -1 --color=always -- "$realpath"'
 # custom fzf flags
 zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
 # To make fzf-tab follow FZF_DEFAULT_OPTS.
