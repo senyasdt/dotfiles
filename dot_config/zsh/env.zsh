af91cdabfcf7ee5b22d58ad67c5ae8e3614fffe0
senyasdt <senyasdt4745@gmail.com>
2026-07-29 15:35:41 +0300

    Fix zsh local bin path setup

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -9,4 +9,6 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
 LS_COLORS="di=1;32:ln=36:ex=1;31:fi=0"
 # export NAVI_PATH=$HOME/.config/navi/cheats
 
-source $HOME/.local/bin/env
+typeset -U path PATH
+path=("$HOME/.local/bin" $path)
+export PATH
